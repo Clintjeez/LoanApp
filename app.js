1fde70b55674c73742b3cff3e4aebea4eb7a4f84
Clinton James <clintjeez@gmail.com>
2019-09-02 11:34:36 +0100

    initial updates

---
+++ app.js
@@ -0,0 +1,75 @@
+// Listen on submit
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+
+  e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults() {
+  console.log('Calculating...');
+  // UI vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calcuatedPayment = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calcuatedPayment);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calcuatedPayment).toFixed(2);
+    totalInterest.value = ((monthly * calcuatedPayment) - principal).toFixed(2);
+
+    // sHOW Results
+    document.getElementById('results').style.display = 'block';
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('Please check your numbers');
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+  }
+
+
+}
+
+// Show Error
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement('div');
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3sec
+  setTimeout(clearErro, 3000);
+
+}
+
+// clear error
+function clearErro() {
+  document.querySelector('.alert').remove();
+}
